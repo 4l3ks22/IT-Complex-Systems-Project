7845489421e2cc328efcfb6ac8f9833a527bec20
cesarorejuela <ceoc@ruc.dk>
2025-11-26 13:56:33 +0100

    Added component Paginatedtitles and Homepage has been modified to only recieve components

---
+++ Frontend/src/components/PaginatedTitles.jsx
@@ -0,0 +1,43 @@
+﻿import {usePaginatedTitles} from "../hooks/usePaginatedTitles.jsx";
+import React from "react";
+
+export default function PaginatedTitles() {
+    const { titles, pageInfo, loading, setUrl } =
+        usePaginatedTitles("http://localhost:5000/api/titles");
+    
+    if (loading) return <p>Loading titles...</p>;
+    
+    return (
+        <div>
+            <h1>All Titles</h1>
+            <ul className="list-group mb-3">
+                {titles.map(t => (
+                    <li key={t.url} className="list-group-item">
+                        <a href={`/titles/${t.url.split("/").pop()}`}>
+                            {t.primarytitle} ({t.startyear})
+                        </a>
+                    </li>
+                ))}
+            </ul>
+    
+            <div className="d-flex gap-2">
+                <button
+                    className="btn btn-secondary"
+                    disabled={!pageInfo.prev}
+                    onClick={() => setUrl(pageInfo.prev)}
+                >
+                    Previous
+                </button>
+    
+                <button
+                    className="btn btn-secondary"
+                    disabled={!pageInfo.next}
+                    onClick={() => setUrl(pageInfo.next)}
+                >
+                    Next
+                </button>
+    
+            </div>
+        </div>
+    );
+}
--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -22,46 +22,13 @@ export default function HomePage() {
 import React from "react";
 import { usePaginatedTitles } from "../hooks/usePaginatedTitles";
 import { usePersons } from "../hooks/usePersons";
-
+import PaginatedTitles from "../components/PaginatedTitles";
 
 export default function HomePage() {
-    const { titles, pageInfo, loading, setUrl } =
-        usePaginatedTitles("http://localhost:5000/api/titles");
-
-    if (loading) return <p>Loading titles...</p>;
-
     return (
-        <div>
-            <h1>All Titles</h1>
-            <ul className="list-group mb-3">
-                {titles.map(t => (
-                    <li key={t.url} className="list-group-item">
-                        <a href={`/titles/${t.url.split("/").pop()}`}>
-                            {t.primarytitle} ({t.startyear})
-                        </a>
-                    </li>
-                ))}
-            </ul>
-
-            <div className="d-flex gap-2">
-                <button
-                    className="btn btn-secondary"
-                    disabled={!pageInfo.prev}
-                    onClick={() => setUrl(pageInfo.prev)}
-                >
-                    Previous
-                </button>
-
-                <button
-                    className="btn btn-secondary"
-                    disabled={!pageInfo.next}
-                    onClick={() => setUrl(pageInfo.next)}
-                >
-                    Next
-                </button>
-                
-            </div>
-        </div>
+        <p>
+            <PaginatedTitles/>
+        </p>
     );
 }
 
